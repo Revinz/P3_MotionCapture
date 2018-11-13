a6d724b085d840707eda9d03586ec22fc5aadabc
JannikPedersen <jclp17@student.aau.dk>
2018-11-13 13:06:50 +0100

    update

--- tf-openpose/run_video.py
+++ tf-openpose/run_video.py
@@ -28,7 +28,7 @@ totalJoints = 0
 
 frameJoints = 0
 
-frame = -1
+frame = 0
 
 jointList = []
 frameList = []
@@ -36,7 +36,7 @@ frameList = []
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
-    parser.add_argument('--video', type=str, default='')
+    parser.add_argument('--video', type=str, default='Test.mp4')
     parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
     parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin')
     parser.add_argument('--show-process', type=bool, default=False,
@@ -53,12 +53,13 @@ if __name__ == '__main__':
         print("Error opening video stream or file")
     while cap.isOpened():
         ret_val, image = cap.read()
-
+        frame = frame + 1
         humans = e.inference(image)
         if not args.showBG:
             image = np.zeros(image.shape)
         image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
 
+        cv2.putText(image, str(frame), (0,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv2.imshow('tf-pose-estimation result', image)
         fps_time = time.time()
@@ -80,8 +81,6 @@ if __name__ == '__main__':
         frameJoints = 0
 
         frameList.append(frame)
-
-        cv2.putText(image, frame, (0,0), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         if cv2.waitKey(1) == 27:
             break
